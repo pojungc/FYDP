720f2648e31ab47be97ad30671f099f8887e1ef3
gananPrabaharan <p.ganan@yahoo.com>
2018-11-14 14:42:41 -0500

    Changed Gear ratios, updated Graph labels

--- gear_logic.py
+++ gear_logic.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Variables:
     Acceleration = 0
     Elevation = 1
@@ -9,8 +10,6 @@ class Variables:
 
 # LOW GEAR = HIGH GEAR RATIO
 
-# Coasting
-# If the user is not pedalling, don't change gear ratio
 
 # ACCELERATION
 # Case 1: Constant Elevation, Constant Acceleration, Constant Friction
@@ -36,37 +35,56 @@ class Variables:
 # Case 7: Constant Elevation, Constant Acceleration, Decreasing Friction
 # Shift to higher gear ratio
 
+
+# SPECIAL CASES
+# Coasting - User not Pedalling
+# If the user is not pedalling, don't change gear ratio
+
+# Bike Speed
+# If Bike is stationary switch to Max gear ratio
+
+
+
+
 class Constants:
     GEAR_RATIO = 1
     WHEEL_RADIUS = 0.311
     PEDAL_SPEED = 80
 
-    MAX_GEAR_RATIO = 2
-    MIN_GEAR_RATIO = 0.5
+    MAX_GEAR_RATIO = 1
+    MIN_GEAR_RATIO = 0.25
     DESIRED_CADENCE = 60
     AVERAGE_SPEED = 40 # km/h
+
+PRINT_FLAG = False
+
 
 def get_wheel_radius():
     return Constants.WHEEL_RADIUS
 
 
-def get_cadence():
-    return Constants.PEDAL_SPEED
+def get_pedal_speed(bike_speed, gear_ratio):
+    bike_speed = float(bike_speed)/3.6*60
+    output_gear_speed = bike_speed/(2*math.pi*Constants.WHEEL_RADIUS)
+    pedal_speed = output_gear_speed * gear_ratio
+    return pedal_speed
 
 
 def get_bike_speed(pedal_speed, gear_ratio):
-    output_gear_speed = pedal_speed / gear_ratio
+    output_gear_speed = float(pedal_speed) / gear_ratio
     bike_speed = 2 * math.pi * Constants.WHEEL_RADIUS * output_gear_speed / 60 * 3.6    # Speed in km/h
     return bike_speed
 
 
-def get_gear_ratio(old_pedal_speed, new_pedal_speed, old_gear_ratio):
-    if old_pedal_speed == 0:
+def get_gear_ratio(new_pedal_speed, old_gear_ratio):
+    if new_pedal_speed == 0:
         new_gear_ratio = Constants.MAX_GEAR_RATIO
     else:
-        #new_gear_ratio = old_gear_ratio*old_pedal_speed/new_pedal_speed
         new_gear_ratio = old_gear_ratio * Constants.DESIRED_CADENCE / new_pedal_speed
 
     new_gear_ratio = min(max(new_gear_ratio, Constants.MIN_GEAR_RATIO), Constants.MAX_GEAR_RATIO)
-    print('New Speed: ', new_pedal_speed, ', Old Speed: ', old_pedal_speed, 'Old Gear Ratio: ', old_gear_ratio, 'New Gear Ratio: ', new_gear_ratio)
+    if PRINT_FLAG:
+        print('New Cadence: ', new_pedal_speed, ', Desired Cadence: ', Constants.DESIRED_CADENCE, 'Old Gear Ratio: ', old_gear_ratio, 'New Gear Ratio: ', new_gear_ratio)
     return new_gear_ratio
+
+# print(get_pedal_speed(20, 0.5))
--- pedal_simulator.py
+++ pedal_simulator.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import gear_logic
 
 
+CHANGE_PEDAL = False
+
+
 def get_index():
     return curr_time % len(pedal_speed)
 
@@ -24,20 +27,40 @@ def change_pedal_speed(delta=None, absolute=None):
     elif absolute is not None:
         pedal_speed[index] = absolute
 
+
+def change_bike_speed(delta=None, absolute=None):
+    global bike_speed
+    global curr_time
+
+    index = curr_time % len(bike_speed)
+
+    if delta is not None:
+        bike_speed[index] = bike_speed[index-1] + delta
+    elif absolute is not None:
+        bike_speed[index] = absolute
+
+
 def add_point():
     global pedal_speed
     global curr_time
     global gear_ratio
 
     index = get_index()
-    ax_pedal.plot(curr_time, pedal_speed[index], 'go')
 
-    gear_ratio = gear_logic.get_gear_ratio(pedal_speed[index-1], pedal_speed[index], gear_ratio)
-    ax_gear.plot(curr_time, gear_ratio, 'bo')
+    if CHANGE_PEDAL:
+        ax_pedal.plot(curr_time, pedal_speed[index], 'go')
+        gear_ratio = gear_logic.get_gear_ratio(pedal_speed[index], gear_ratio)
+        ax_gear.plot(curr_time, gear_ratio, 'bo')
 
-    ax_speed.plot(curr_time, gear_logic.get_bike_speed(pedal_speed[index], gear_ratio), 'ro')
+        ax_speed.plot(curr_time, gear_logic.get_bike_speed(pedal_speed[index], gear_ratio), 'ro')
+    else:
+        ax_speed.plot(curr_time, bike_speed[index], 'ro')
+        pedal_speed[index] = gear_logic.get_pedal_speed(bike_speed[index], gear_ratio)
+        gear_ratio = gear_logic.get_gear_ratio(pedal_speed[index], gear_ratio)
+        ax_gear.plot(curr_time, gear_ratio, 'bo')
 
-    print(pedal_speed)
+        ax_pedal.plot(curr_time, pedal_speed[index], 'go')
+
     fig.canvas.draw()
 
 
@@ -50,35 +73,50 @@ def press(event):
     if event.key == 'z':
         if available:
             available = False
-            change_pedal_speed(delta=-1)
+
+            if CHANGE_PEDAL:
+                change_pedal_speed(delta=-1)
+            else:
+                change_bike_speed(delta=-1)
+
             add_point()
             increase_time()
             available = True
     elif event.key == 'x':
         if available:
             available = False
-            change_pedal_speed(delta=0)
+            if CHANGE_PEDAL:
+                change_pedal_speed(delta=0)
+            else:
+                change_bike_speed(delta=0)
             add_point()
             increase_time()
             available = True
     elif event.key == 'c':
         if available:
             available = False
-            change_pedal_speed(delta=1)
+            if CHANGE_PEDAL:
+                change_pedal_speed(delta=1)
+            else:
+                change_bike_speed(delta=1)
             add_point()
             increase_time()
             available = True
     elif event.key == 'v':
         if available:
             available = False
-            change_pedal_speed(absolute=0)
+            if CHANGE_PEDAL:
+                change_pedal_speed(absolute=0)
+            else:
+                change_bike_speed(absolute=0)
             add_point()
             increase_time()
             available = True
 
 available = True
 
-pedal_speed = np.zeros(5)+30
+pedal_speed = np.zeros(5)  #+30
+bike_speed = np.zeros(5)
 curr_time = 0
 gear_ratio = 0
 
@@ -87,16 +125,29 @@ fig.canvas.mpl_connect('key_press_event', press)
 
 ax_pedal = axes[0]
 ax_speed = axes[1]
+title = 'Desired Cadence: ' + str(gear_logic.Constants.DESIRED_CADENCE) + ' RPM, Gear Ratios: ' + \
+        str(gear_logic.Constants.MIN_GEAR_RATIO) + '-' + str(gear_logic.Constants.MAX_GEAR_RATIO)
 
-ax_pedal.set_title('Press a key')
+ax_pedal.set_title(title)
 ax_pedal.set_xlim([0, 100])
-ax_pedal.set_ylim([0, 100])
+ax_pedal.set_ylim([0, 200])
+ax_pedal.plot(0, 0, 'go')
+ax_pedal.legend(['Biker Cadence'], loc=2)
+ax_pedal.set_xlabel('Time (s)')
+ax_pedal.set_ylabel('Cadence (rpm)')
 
 ax_gear = ax_pedal.twinx()
 ax_gear.set_xlim([0, 100])
 ax_gear.set_ylim([0, 2])
+ax_gear.plot(0, 0, 'bo')
+ax_gear.legend(['Gear Ratio'], loc=1)
+ax_gear.set_ylabel('Gear Ratio')
 
 ax_speed.set_xlim([0, 100])
 ax_speed.set_ylim([0, 60])
+ax_speed.plot(0, 0, 'ro')
+ax_speed.legend(['Bike Speed'])
+ax_speed.set_xlabel('Time (s)')
+ax_speed.set_ylabel('Biker Speed (km/h)')
 
 plt.show()
